7a928863f6f01a55b39a07e2fab31cf0e01379a8
Aya Nakazawa <nkzw0817@gmail.com>
2017-04-04 01:09:15 +0900

    [add] Add process #setting-view-column-area input
    about #setting-view-column-area input
      in js
      add process
        checked: add .display-none to .change-value, .difference-of-above
        unchecked: add .display-none to .change-value, .difference-of-above
    add style #ranking-area .display-none

--- js/myscript.js
+++ js/myscript.js
@@ -8,7 +8,22 @@
 $(function() {
   
   // ----------------------------------------------------------------
-  // 
+  // View difference columnを変更
+  $(document).on("change", "#setting-view-column-area input", function() {
+    console.log("Change Vire difference column: " + $(this).prop("checked"));
+    
+    var checkbox = this;
+    
+    $(".change-value, .difference-of-above").each(function(i, element) {
+      if ($(checkbox).prop("checked")) {
+        $(element).removeClass("display-none");
+        
+      } else {
+        $(element).addClass("display-none");
+        
+      }
+    });
+  });
   
 });
 
