505f1c758b7b6dc87144edbdb891eea6af06d480
Aya Nakazawa <nkzw0817@gmail.com>
2017-04-05 23:02:38 +0900

    [add] Add function getDateString
    about getDateString
      in js
      string getDateString(Date _date, string _format)
      initial value
        _date: new Date()
        _format: '%Y/%m/%d %H:%M:%S'
      arguments
        _date
          Date object
        _format
          %Y: 4 digits year
          %y: 2 digits year
          %m: 2 digits month
          %d: 2 digits day
          %H: 2 digits hour
          %M: 2 digits minute
          %S: 2 digits second

--- js/myscript.js
+++ js/myscript.js
@@ -39,6 +39,49 @@ $(function() {
 // Functions
 
 // ----------------------------------------------------------------
+// 日付一覧を取得
+
+// ----------------------------------------------------------------
+// Dateオブジェクトからゼロ埋めした日付文字列を生成
+// _date: new Date()
+//  Dateオブジェクト
+// _format: 0
+//  %Y: 年4桁
+//  %y: 年2桁
+//  %m: 月
+//  %d: 日
+//  %H: 時
+//  %M: 分
+//  %S: 秒
+function getDateString(_date, _format){
+  var date = _date || new Date();
+  var dateString = _format || '%Y/%m/%d %H:%M:%S';
+  
+  if (dateString.indexOf('%Y') >= 0) {
+    dateString = dateString.replace('%Y', ("000" + _date.getFullYear()).slice(-4));
+  }
+  if (dateString.indexOf('%y') >= 0) {
+    dateString = dateString.replace('%y', ("0" + _date.getFullYear()).slice(-2));
+  }
+  if (dateString.indexOf('%m') >= 0) {
+    dateString = dateString.replace('%m', ("0" + (_date.getMonth() + 1)).slice(-2));
+  }
+  if (dateString.indexOf('%d') >= 0) {
+    dateString = dateString.replace('%d', ("0" + _date.getDate()).slice(-2));
+  }
+  if (dateString.indexOf('%H') >= 0) {
+    dateString = dateString.replace('%H', ("0" + _date.getHours()).slice(-2));
+  }
+  if (dateString.indexOf('%M') >= 0) {
+    dateString = dateString.replace('%M', ("0" + _date.getMinutes()).slice(-2));
+  }
+  if (dateString.indexOf('%S') >= 0) {
+    dateString = dateString.replace('%S', ("0" + _date.getSeconds()).slice(-2));
+  }
+  return dateString;
+}
+
+// ----------------------------------------------------------------
 // true false をスイッチ
 function toggleBoolean(_bool) {
   return !_bool;
