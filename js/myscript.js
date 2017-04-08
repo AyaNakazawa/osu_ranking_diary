c0147a4da0fc88272fb9057aecaa4f83bb055093
Aya Nakazawa <nkzw0817@gmail.com>
2017-04-09 01:49:19 +0900

    [modify] add generate init & exit ranking
    generate init & exit ranking

--- js/myscript.js
+++ js/myscript.js
@@ -19,8 +19,11 @@ var exitDate;
 var initRank;
 var exitRank;
 
-var initRanking = new Array([]);
-var exitRanking = new Array([]);
+var initRanking = new Array();
+var exitRanking = new Array();
+
+var initPlayer = new Array();
+var exitPlayer = new Array();
 
 
 // 定数
@@ -34,6 +37,7 @@ $(function() {
   initializeORD();
   
   $(document).ajaxSuccess(function() {
+    console.log("initializeStatus: " + initializeStatus + ", loadRankingStatus: " + loadRankingStatus);
     if (initializeStatus === 1) {
       initializeORD();
       
@@ -334,46 +338,136 @@ function buildRanking() {
   var tdDiffA = '<td class="change-value type-int display-none"></td>';
   var tdDiffSSA = '<td class="change-value type-int display-none"></td>';
   
-  if (diffStatus) {
-    console.log("initRanking: ");
-    console.log(initRanking);
-    
-    tdArrow = '<td class="direction-arrow">';
-    tdDiffRank = '<td class="change-value type-int">';
-    tdDiffPPAbove = '<td class="difference-of-above">';
-    tdDiffAcc = '<td class="change-value type-percentage">';
-    tdDiffPlay = '<td class="change-value type-int">';
-    tdDiffBP = '<td class="change-value type-int">';
-    tdDiffBonus = '<td class="change-value type-float">';
-    tdDiffPP = '<td class="change-value type-int">';
-    tdDiffSS = '<td class="change-value type-int">';
-    tdDiffS = '<td class="change-value type-int">';
-    tdDiffA = '<td class="change-value type-int">';
-    tdDiffSSA = '<td class="change-value type-int">';
-    
-  }
-  
-  console.log("exitRanking: ");
-  console.log(exitRanking);
-  
   for (var i = initRank - 1; i <= exitRank - 1; i++) {
     
-    var ssaLocal = 1 * exitRanking[i][5] + exitRanking[i][6] + exitRanking[i][7];
+    var ssaLocalExit = 1 * exitRanking[exitPlayer[i]][5] + exitRanking[exitPlayer[i]][6] + exitRanking[exitPlayer[i]][7];
     // 416.6667 (1- 0.9994^Number_of_scores)
-    var bonusLocal = 416.6667 * (1 - Math.pow(0.9994, ssaLocal));
-    var bpLocal = Math.floor(1 * exitRanking[i][4] - bonusLocal);
+    var bonusLocalExit = 416.6667 * (1 - Math.pow(0.9994, ssaLocalExit));
+    var bpLocalExit = Math.floor(1 * exitRanking[exitPlayer[i]][4] - bonusLocalExit);
     
-    tdRank = '<td class="current-value type-rank">#' + exitRanking[i][0].toLocaleString() + '</td>';
-    tdName = '<td class="current-value type-string type-name" data-user-id="' + exitRanking[i][8] + '">' + exitRanking[i][1] + '</td>';
-    tdAcc = '<td class="current-value type-percentage">' + exitRanking[i][2] + '%</td>';
-    tdPlay = '<td class="current-value type-int">' + exitRanking[i][3].toLocaleString() + '</td>';
-    tdBP = '<td class="current-value type-int">' + bpLocal.toLocaleString() + '</td>';
-    tdBonus = '<td class="current-value type-float">' + bonusLocal.toLocaleString() + '</td>';
-    tdPP = '<td class="current-value type-int">' + exitRanking[i][4].toLocaleString() + '</td>';
-    tdSS = '<td class="current-value type-int">' + exitRanking[i][5].toLocaleString() + '</td>';
-    tdS = '<td class="current-value type-int">' + exitRanking[i][6].toLocaleString() + '</td>';
-    tdA = '<td class="current-value type-int">' + exitRanking[i][7].toLocaleString() + '</td>';
-    tdSSA = '<td class="current-value type-int">' + ssaLocal.toLocaleString() + '</td>';
+    tdRank = '<td class="current-value type-rank">#' + exitRanking[exitPlayer[i]][0].toLocaleString() + '</td>';
+    tdName = '<td class="current-value type-string type-name" data-user-id="' + exitRanking[exitPlayer[i]][8] + '">' + exitRanking[exitPlayer[i]][1] + '</td>';
+    tdAcc = '<td class="current-value type-percentage">' + exitRanking[exitPlayer[i]][2].toFixed(2) + '%</td>';
+    tdPlay = '<td class="current-value type-int">' + exitRanking[exitPlayer[i]][3].toLocaleString() + '</td>';
+    tdBP = '<td class="current-value type-int">' + bpLocalExit.toLocaleString() + '</td>';
+    tdBonus = '<td class="current-value type-float">' + bonusLocalExit.toFixed(3).toLocaleString() + '</td>';
+    tdPP = '<td class="current-value type-int">' + exitRanking[exitPlayer[i]][4].toLocaleString() + '</td>';
+    tdSS = '<td class="current-value type-int">' + exitRanking[exitPlayer[i]][5].toLocaleString() + '</td>';
+    tdS = '<td class="current-value type-int">' + exitRanking[exitPlayer[i]][6].toLocaleString() + '</td>';
+    tdA = '<td class="current-value type-int">' + exitRanking[exitPlayer[i]][7].toLocaleString() + '</td>';
+    tdSSA = '<td class="current-value type-int">' + ssaLocalExit.toLocaleString() + '</td>';
+    
+    if (diffStatus) {
+      
+      var arrowLocal = '<i class="fa fa-arrow-up" aria-hidden="true"></i>';
+      var diffRankLocal = '+' + (1001 - exitRanking[exitPlayer[i]][0]) + '~';
+      var diffPPAboveLocal;
+      var diffAccLocal = "";
+      var diffPlayLocal = "";
+      var diffBPLocal = "";
+      var diffBonusLocal = "";
+      var diffPPLocal = "";
+      var diffSSLocal = "";
+      var diffSLocal = "";
+      var diffALocal = "";
+      var diffSSALocal = "";
+      
+      if (initRanking[exitPlayer[i]] != null) {
+        var ssaLocalInit = 1 * initRanking[exitPlayer[i]][5] + initRanking[exitPlayer[i]][6] + initRanking[exitPlayer[i]][7];
+        var bonusLocalInit = 416.6667 * (1 - Math.pow(0.9994, ssaLocalInit));
+        var bpLocalInit = Math.floor(1 * initRanking[exitPlayer[i]][4] - bonusLocalInit);
+        
+        arrowLocal = "";
+        diffRankLocal = initRanking[exitPlayer[i]][0] - exitRanking[exitPlayer[i]][0];
+        diffPPAboveLocal = "";
+        diffAccLocal = exitRanking[exitPlayer[i]][2] - initRanking[exitPlayer[i]][2];
+        diffPlayLocal = exitRanking[exitPlayer[i]][3] - initRanking[exitPlayer[i]][3];
+        diffBPLocal = bpLocalExit - bpLocalInit;
+        diffBonusLocal = bonusLocalExit - bonusLocalInit;
+        diffPPLocal = exitRanking[exitPlayer[i]][4] - initRanking[exitPlayer[i]][4];
+        diffSSLocal = exitRanking[exitPlayer[i]][5] - initRanking[exitPlayer[i]][5];
+        diffSLocal = exitRanking[exitPlayer[i]][6] - initRanking[exitPlayer[i]][6];
+        diffALocal = exitRanking[exitPlayer[i]][7] - initRanking[exitPlayer[i]][7];
+        diffSSALocal = ssaLocalExit - ssaLocalInit;
+        
+        if (diffRankLocal === 0) {
+          arrowLocal = '<i class="fa fa-arrow-right" aria-hidden="true"></i>';
+          diffRankLocal = "";
+        } else if (diffRankLocal > 0) {
+          arrowLocal = '<i class="fa fa-arrow-up" aria-hidden="true"></i>';
+          diffRankLocal = "+" + diffRankLocal.toLocaleString();
+        } else if (diffRankLocal < 0) {
+          arrowLocal = '<i class="fa fa-arrow-down" aria-hidden="true"></i>';
+        }
+        
+        if (diffAccLocal > 0) {
+          diffAccLocal = "+" + diffAccLocal.toFixed(2) + "%";
+        } else {
+          diffAccLocal = diffAccLocal.toFixed(2) + "%";
+        }
+        if (diffPlayLocal > 0) {
+          diffPlayLocal = "+" + diffPlayLocal.toLocaleString();
+        } else {
+          diffPlayLocal = diffPlayLocal.toLocaleString();
+        }
+        if (diffBPLocal > 0) {
+          diffBPLocal = "+" + diffBPLocal.toLocaleString();
+        } else {
+          diffBPLocal = diffBPLocal.toLocaleString();
+        }
+        if (diffBonusLocal > 0) {
+          diffBonusLocal = "+" + diffBonusLocal.toFixed(3).toLocaleString();
+        } else {
+          diffBonusLocal = diffBonusLocal.toFixed(3).toLocaleString();
+        }
+        if (diffPPLocal > 0) {
+          diffPPLocal = "+" + diffPPLocal.toLocaleString();
+        } else {
+          diffPPLocal = diffPPLocal.toLocaleString();
+        }
+        if (diffSSLocal > 0) {
+          diffSSLocal = "+" + diffSSLocal.toLocaleString();
+        } else {
+          diffSSLocal = diffSSLocal.toLocaleString();
+        }
+        if (diffSLocal > 0) {
+          diffSLocal = "+" + diffSLocal.toLocaleString();
+        } else {
+          diffSLocal = diffSLocal.toLocaleString();
+        }
+        if (diffALocal > 0) {
+          diffALocal = "+" + diffALocal.toLocaleString();
+        } else {
+          diffALocal = diffALocal.toLocaleString();
+        }
+        if (diffSSALocal > 0) {
+          diffSSALocal = "+" + diffSSALocal.toLocaleString();
+        } else {
+          diffSSALocal = diffSSALocal.toLocaleString();
+        }
+      }
+      // pp above
+      if (i === initRank - 1) {
+        diffPPAboveLocal = "TOP";
+      } else {
+        diffPPAboveLocal = exitRanking[exitPlayer[i]][4] - exitRanking[exitPlayer[i - 1]][4];
+        diffPPAboveLocal = diffPPAboveLocal.toLocaleString();
+      }
+      
+      tdArrow = '<td class="direction-arrow">' + arrowLocal + '</td>';
+      tdDiffRank = '<td class="change-value type-int">' + diffRankLocal + '</td>';
+      tdDiffPPAbove = '<td class="difference-of-above">' + diffPPAboveLocal + '</td>';
+      tdDiffAcc = '<td class="change-value type-percentage">' + diffAccLocal + '</td>';
+      tdDiffPlay = '<td class="change-value type-int">' + diffPlayLocal + '</td>';
+      tdDiffBP = '<td class="change-value type-int">' + diffBPLocal + '</td>';
+      tdDiffBonus = '<td class="change-value type-float">' + diffBonusLocal + '</td>';
+      tdDiffPP = '<td class="change-value type-int">' + diffPPLocal + '</td>';
+      tdDiffSS = '<td class="change-value type-int">' + diffSSLocal + '</td>';
+      tdDiffS = '<td class="change-value type-int">' + diffSLocal + '</td>';
+      tdDiffA = '<td class="change-value type-int">' + diffALocal + '</td>';
+      tdDiffSSA = '<td class="change-value type-int">' + diffSSALocal + '</td>';
+      
+    }
     
     tr = '<tr>';
     tr += tdArrow;
@@ -463,7 +557,7 @@ function getRanking(_date, _initRank, _exitRank) {
       // console.log(_json);
       // console.log("_textStatus: " + _textStatus);
       
-      var dateSwitch;
+      var dateSwitch = true;
       if (_json["date"] === $("#init-date").val()) {
         dateSwitch = true;
       } else if (_json["date"] === $("#exit-date").val()) {
@@ -475,6 +569,7 @@ function getRanking(_date, _initRank, _exitRank) {
           if (i === 0) {
             return true;
           }
+          
           var rankLocal = 1 * _json[playerKey]["rank"];
           var nameLocal = _json[playerKey]["name"];
           var accLocal = 1 * _json[playerKey]["acc"];
@@ -496,10 +591,12 @@ function getRanking(_date, _initRank, _exitRank) {
           player.push(aLocal);
           player.push(idLocal);
           
-          if (!dateSwitch) {
-            initRanking[rankLocal - 1] = player;
+          if (dateSwitch) {
+            initRanking["" + idLocal] = player;
+            initPlayer[rankLocal - 1] = "" + idLocal;
           } else {
-            exitRanking[rankLocal - 1] = player;
+            exitRanking["" + idLocal] = player;
+            exitPlayer[rankLocal - 1] = "" + idLocal;
           }
         });
         
